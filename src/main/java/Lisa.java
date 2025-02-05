930ef02d4c6b470efcaec836f83c56d288f7c1ef
XMTAN\xmtan <xmtan9999@gmail.com>
2025-02-05 18:14:16 +0800

    renamed Duke

---
+++ src/main/java/Lisa.java
@@ -0,0 +1,8 @@
+public class Lisa {
+    public static void main(String[] args) {
+        String name = "Lisa";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
