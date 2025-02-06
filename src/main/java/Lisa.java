2b605166b205126e2e639e274d62b19d7ada316f
XMTAN\xmtan <xmtan9999@gmail.com>
2025-02-06 10:00:57 +0800

    add list

--- src/main/java/Lisa.java
+++ src/main/java/Lisa.java
@@ -5,12 +5,31 @@ public class Lisa {
         String name = "Lisa";
         System.out.println("Hiya! I'm " + name);
         System.out.println("What can I do for you?");
+
+        String[] requests = new String[100];
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
+
+        int i = 0;
         while (!word.equals("bye")) {
-            System.out.println(word);
-            word = sc.nextLine();
+            if (word.equals("list")) {
+                printList(requests);
+                word = sc.nextLine();
+            } else {
+                requests[i] = word;
+                System.out.println("added: " + word);
+                word = sc.nextLine();
+                i++;
+            }
         }
         System.out.println("Bye! See ya next time :)");
+    }
+
+    public static void printList(String[] requests) {
+        int i = 0;
+        while (requests[i] != null) {
+            System.out.println(i+1 + ". " + requests[i]);
+            i++;
+        }
     }
 }
