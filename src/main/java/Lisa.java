c9e4352e48e7a98c8f5d18736438ffc6af2e7592
XMTAN\xmtan <xmtan9999@gmail.com>
2025-02-06 09:37:25 +0800

    echo command

--- src/main/java/Lisa.java
+++ src/main/java/Lisa.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Lisa {
     public static void main(String[] args) {
         String name = "Lisa";
-        System.out.println("Hello! I'm " + name);
+        System.out.println("Hiya! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(word);
+            word = sc.nextLine();
+        }
+        System.out.println("Bye! See ya next time :)");
     }
 }
