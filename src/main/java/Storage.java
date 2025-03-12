5a3d4662795f5ea47ff320fe693587c9785597b3
XMTAN\xmtan <xmtan9999@gmail.com>
2025-03-12 17:58:22 +0800

    modify Storage class JavaDoc

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -5,19 +5,28 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
- * Stores the list into a hard disk when list is changed and
- * loads stored list when Lisa is started.
- * A <code>Storage</code> object contains the relative file path
- * of the hard disk
+ * The Storage class manages the storage of tasks.
+ * This class provides methods to save tasks to and load tasks from a file.
+ * The tasks are stored in a file whose path is provided during instantiation.
  *
  */
 public class Storage {
     private String filePath;
 
+    /**
+     * Constructs a Storage object with a specified file path.
+     *
+     * @param filePath the path of the file were tasks will be saved or loaded from.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Loads tasks from the file specified by filePath and adds them to a list of tasks.
+     *
+     * @param tasks the list to which the tasks read from the file will be added to.
+     */
     public void transferSavedData(ArrayList<Task> tasks) {
         File savedTasks = new File(filePath);
         try {
@@ -31,6 +40,15 @@ public class Storage {
     }
 
     //"data/Lisa.txt"
+
+    /**
+     * Saves the list of tasks to the file specified by filePath.
+     * If the file does not exist, it will create the necessary directory
+     * and file before saving.
+     * If the file exists, it will overwrite the existing content.
+     *
+     * @param tasks the list of tasks to be saved to the file.
+     */
     public void saveTasks(ArrayList<Task> tasks) {
         File savedTasks = new File(filePath);
         try {
